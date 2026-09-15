4fe7cad977620305f7febf92900ec880956ddf91
Remco <remcowiersema@gmail.com>
2026-09-15 16:41:20 +0200

    set up reports for github workflows

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'line',
+  reporter: [
+    ['allure-playwright', { outputFolder: 'test-results' }],
+    ['line']
+  ],
   // reporter: 'html',
 
   globalSetup: './utils/global-setup',
