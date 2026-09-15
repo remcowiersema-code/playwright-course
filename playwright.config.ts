b8a30f95d3bb71e97ccac9001bf50aeda68d5242
Remco <remcowiersema@gmail.com>
2026-09-15 16:48:36 +0200

    set up reports for github workflows - poging 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     baseURL: 'https://practice.sdetunicorns.com/blog',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on-first-retry',
 
     storageState: 'loggedInState.json'
   },
